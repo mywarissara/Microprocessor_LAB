b57bed3ecddccd53f6bb7688878ab5fcc78ea5af
warissara <warissara.cc@gmail.com>
2020-02-24 15:50:35 +0700

    wk6

--- Wk6/Wk6-Q2/Wk6-Q2.ino
+++ Wk6/Wk6-Q2/Wk6-Q2.ino
@@ -1,31 +1,20 @@
-#include <ThreeWire.h>  
+/*
+ * The time from program and time from timer clock is almost the same 
+ * difference of it is only 0.14 sec 
+ * I think it happens because of the error between the program
+ * but it is very little so it might not be recognize 
+ * 
+ * warissara 61010946
+ * 
+ */#include <ThreeWire.h>  
 #include <RtcDS1302.h>
 #define led 6
 ThreeWire myWire(4,5,2); // IO, SCLK, CE
 RtcDS1302<ThreeWire>Rtc(myWire);
 int sectime =0, sectime_begin;
-//void printDateTime(const RtcDateTime& dt){
-//  Serial.print(dt.Year(), DEC);
-//  Serial.print('/');
-//  Serial.print(dt.Month(), DEC);
-//  Serial.print('/');
-//  Serial.print(dt.Day(), DEC);
-//  Serial.print(' ');
-//  Serial.print(dt.Hour(), DEC);
-//  Serial.print(':');
-//  Serial.print(dt.Minute(), DEC);
-//  Serial.print(':');
-//  Serial.print(dt.Second(), DEC);
-//  Serial.println();
-//  Serial.print("Total Seconds since 1/1/2000 : ");
-//  Serial.print(dt.TotalSeconds());
-//  Serial.println();
-//  }
 void setup() {
   Serial.begin(9600);
   Rtc.Begin();
-  RtcDateTime setting_time(2020,2,2,23,31,10);// year, month, dayOfMonth, hour, minute, second
-  Rtc.SetDateTime(setting_time);
   Serial.println("Start");
   pinMode(led, OUTPUT);
   RtcDateTime now = Rtc.GetDateTime();
